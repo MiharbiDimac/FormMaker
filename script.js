4e0815e15eeaaae9910b79d859658c88c03f9d30
MiharbiDimac <85240201+MiharbiDimac@users.noreply.github.com>
2023-05-11 06:16:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+const insert = () => {
+
+    let type = document.querySelector('#type').value 
+    let label = document.querySelector('#label').value 
+
+    switch(type){
+        case 'text': appendText(label)
+            break;
+        case 'date': appendDate(label);
+            break;
+        case 'radio' : appendRadio(label)
+            break;
+        case 'checkbox': appendCheckbox(label);
+            break;
+        case 'button': appendButton(label)
+            break
+        case 'submit': appendSubmit(label);
+            break;
+    }
+
+}
+
+const appendText = (label = "Default") => {
+
+    let elem = document.createElement('input')
+
+    elem.setAttribute('placeholder', label)
+    elem.setAttribute('class', 'form-control mt-2')
+
+    document.querySelector('#display').append(elem)
+
+}
+
+const appendDate = (label = 'Default') => {
+    
+    let elemLabel = document.createElement('label');
+    elemLabel.setAttribute('for', label);
+    elemLabel.textContent = label;
+
+    let elem = document.createElement('input');
+    elem.setAttribute('type', 'date');
+    elem.setAttribute('id', label);
+    elem.setAttribute('name', label);
+    elem.setAttribute('class', 'form-control mt-2');
+
+    let container = document.createElement('div');
+    container.setAttribute('class', 'form-group');
+    container.append(elemLabel, elem);
+    
+    document.querySelector('#display').append(container);  
+
+};
+
+const appendRadio = (label = 'Default') => {
+    let elem = document.createElement('input');
+    elem.setAttribute('type', 'radio');
+    elem.setAttribute('id', label);
+    elem.setAttribute('name', 'radio-group');
+    elem.setAttribute('class', 'form-check-input mt-2');
+  
+    let elemLabel = document.createElement('label');
+    elemLabel.setAttribute('for', label);
+    elemLabel.textContent = label;
+  
+    let container = document.createElement('div');
+    container.setAttribute('class', 'form-check');
+    container.append(elem, elemLabel);
+  
+    document.querySelector('#display').append(container);
+  
+   
+  };
+  
+const appendCheckbox = (label = 'Default') => {
+  
+    let elem = document.createElement('input');
+    elem.setAttribute('type', 'checkbox');
+    elem.setAttribute('id', label);
+    elem.setAttribute('name', label);
+    elem.setAttribute('class', 'form-check-input mt-2');
+
+    let elemLabel = document.createElement('label');
+    elemLabel.setAttribute('for', label);
+    elemLabel.textContent = label;
+
+    let container = document.createElement('div');
+    container.setAttribute('class', 'form-check');
+    container.append(elem, elemLabel);
+
+    document.querySelector('#display').append(container);
+
+};
+
+const appendWhitespace = (label = "Default") => {          
+  
+    let elemP = document.createElement('p');
+    elemP.setAttribute('for', p);
+    elemP.textContent = label
+
+}
+
+const appendButton = (label = "Default") => {
+
+    let elem = document.createElement('button')
+    elem.setAttribute('class', 'btn btn-primary form-control mt-2')
+    elem.textContent = label
+
+    document.querySelector('#display').append(elem)
+
+}
+
+const appendSubmit = (label = 'Submit') => {
+  
+    let elem = document.createElement('input');
+    elem.setAttribute('type', 'submit');
+    elem.setAttribute('value', label);
+    elem.setAttribute('class', 'btn btn-success form-control mt-2');
+    document.querySelector('#display').append(elem);
+
+};
+
+const remove = () => {
+    let display = document.querySelector('#display');
+    let lastChild = display.lastElementChild;
+    if (lastChild) {
+      display.removeChild(lastChild);
+    }
+  };
